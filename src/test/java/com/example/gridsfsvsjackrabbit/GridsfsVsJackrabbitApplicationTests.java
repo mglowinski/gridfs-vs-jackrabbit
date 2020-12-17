63d87d64cbd43bae6709a360a8129856c46bc5ed
Maciej Głowiński <maciej.glowinski@comarch.pl>
2020-12-17 11:25:32 +0100

    init project

---
+++ src/test/java/com/example/gridsfsvsjackrabbit/GridsfsVsJackrabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gridsfsvsjackrabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridsfsVsJackrabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
